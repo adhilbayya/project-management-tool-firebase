6bb5f236c502aabe948c62738b07a1b29286473e
Adhil Mohammed <adhilvk445@gmail.com>
2025-10-30 02:17:02 +0530

    deleted clerk auth

--- project-management-tool-client/src/utils/api.ts
+++ project-management-tool-client/src/utils/api.ts
@@ -1,7 +1,7 @@
 // src/utils/api.ts
 import { auth } from "../firebase";
 
-const API_BASE = import.meta.env.VITE_API_LOCAL_URL;
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export const api = async <T = unknown>(
   path: string,
